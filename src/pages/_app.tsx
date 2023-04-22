f269f8ae283df51247744ef522ec77456852d1ba
Ali <aliehyaie9469@gmail.com>
2023-04-22 15:34:53 +0430

    initial configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.scss'
+import type {AppProps} from 'next/app'
+import {Lato} from 'next/font/google';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const lato = Lato({
+    weight: ['100', '300', '400', '700', '900'],
+    style: ['normal', 'italic'],
+    subsets: ['latin'],
+})
+
+export default function App({Component, pageProps}: AppProps) {
+    return <>
+        <style jsx global>{`
+          html {
+            font-family: ${lato.style.fontFamily};
+          }
+        `}</style>
+        <Component {...pageProps} />
+    </>
 }
